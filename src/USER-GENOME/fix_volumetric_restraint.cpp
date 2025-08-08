8a1a4cbb4d0bffda516c7f51cd3cf2f706ec3cb9
Lorenzo Boninsegna <lorenzobonimba@gmail.com>
2025-08-07 17:43:09 -0700

    upgraded restraint for volume and damim

--- src/USER-GENOME/fix_volumetric_restraint.cpp
+++ src/USER-GENOME/fix_volumetric_restraint.cpp
@@ -45,10 +45,9 @@ FixVolumetricRestraint::FixVolumetricRestraint(LAMMPS *lmp, int narg, char **arg
 {
 
   // preamble
-  int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-  int                          count = 0;
-  double                   new_center[3];
-  double                         scaling;
+  int      s, cell_id, tot_size;
+  double                scaling;
+
 
   //std::ofstream myfile;
   //myfile.open ("debug.txt");
@@ -68,27 +67,17 @@ FixVolumetricRestraint::FixVolumetricRestraint(LAMMPS *lmp, int narg, char **arg
   /*------- read in file whose name is given in "arg[3]", STRUCT object ---- */
   std::ifstream fp(arg[3], std::ios::binary);
 
-  fp >>                                           body_idx;
-  fp >>     n_voxel[0] >>     n_voxel[1] >>     n_voxel[2];  // nx, ny, nz (number of pixels in each direction)
-  fp >>      center[0] >>      center[1] >>      center[2];  // center of mass of the map
-  fp >>      origin[0] >>      origin[1] >>      origin[2];  // origin of the map (bottom, most left pixel edge)
-  fp >>   grid_step[0] >>   grid_step[1] >>   grid_step[2];  // step size of a pixel/voxel
+  // read first few bytes from file: grid obhect
+  //grid_info grid;
+  fp.read((char*) (&grid), sizeof(grid_info));
 
-  // define the size of the arrays
+  // use number of voxels to define the mappa
+  tot_size = grid.nx * grid.ny * grid.nz * 4;
 
-  //myfile << grid_step[0] << " " << grid_step[1] << " " << grid_step[2] << "\n";
+  // define big array and read in one shot
+  mappa = new int[tot_size];
+  fp.read((char*) (mappa), tot_size * sizeof(int));
 
-  /* read in all the entries of the volume map matrix */
-  for (int i = 0; i < n_voxel[0] * n_voxel[1] * n_voxel[2]; i++)
-      {
-	fp >> tmp1 >> tmp2 >> tmp3 >> tmp4 >> tmp5 >> tmp6 >> tmp7;    // one full line at the time, each line is (x, y, z, EDT[x], EDT[y], EDT[z], in/out label)
- 
-        mappa[tmp1][tmp2][tmp3][0] = tmp4;    // reproduce the Euclidean Distance Transform pattern
-        mappa[tmp1][tmp2][tmp3][1] = tmp5;
-        mappa[tmp1][tmp2][tmp3][2] = tmp6;
-        mappa[tmp1][tmp2][tmp3][3] = tmp7;
- 
-      } 
 
   // scaling factors for volumes: for the lamina DamID it would help to have a slightly smaller map, so that there is un intercapetine in cui le particelle sono libere e,
   // anche se non a contatto fisico con la lamina, assumiamo siano a contatto (questo e' il contact range equivalent)
@@ -96,30 +85,49 @@ FixVolumetricRestraint::FixVolumetricRestraint(LAMMPS *lmp, int narg, char **arg
   //if (((body_idx == 0) && (k > 0)) || ((body_idx == 1) && (k < 0))) { scaling = evf;}
 
   // lamina damid with nuclear lamina: need a somehow smaller volume
-  if ((body_idx == 0) && (k < 0)) {scaling = evf * 0.95;}
-  if ((body_idx == 0) && (k > 0)) {scaling = evf;}
+  if ((grid.body_idx == 0) && (k < 0)) {scaling = evf / 0.99;} 
+  if ((grid.body_idx == 0) && (k > 0)) {scaling = evf * 0.99;}
 
-  if ((body_idx == 1) && (k < 0)) {scaling = evf;}
-  if ((body_idx == 1) && (k > 0)) {scaling = evf / 0.95;}
+  if ((grid.body_idx == 1) && (k > 0)) {scaling = evf / 0.99;}
+  if ((grid.body_idx == 1) && (k < 0)) {scaling = evf / 0.99;}
   
-  center[0]     =     center[0] * scaling;
-  center[1]     =     center[1] * scaling;
-  center[2]     =     center[2] * scaling;
+  grid.x0  =  grid.x0 * scaling;
+  grid.y0  =  grid.y0 * scaling;
+  grid.z0  =  grid.z0 * scaling;
 
-  origin[0]     =     origin[0] * scaling;
-  origin[1]     =     origin[1] * scaling;
-  origin[2]     =     origin[2] * scaling;
+  grid.x1  =  grid.x1 * scaling;
+  grid.y1  =  grid.y1 * scaling;
+  grid.z1  =  grid.z1 * scaling;
 
-  grid_step[0]  =  grid_step[0] * scaling;
-  grid_step[1]  =  grid_step[1] * scaling;
-  grid_step[2]  =  grid_step[2] * scaling;
-  	
+  grid.dx  =  grid.dx * scaling;
+  grid.dy  =  grid.dy * scaling;
+  grid.dz  =  grid.dz * scaling;
+  
+  /*myfile << scaling << " " << evf << "\n";
 
-  //myfile << center[0] << " " << center[1] << " " << center[2] << "\n";
-  //myfile << origin[0] << " " << origin[1] << " " << origin[2] << "\n";
-  //myfile << grid_step[0] << " " << grid_step[1] << " " << grid_step[2] << "\n";
+  myfile << grid.body_idx << " " << scaling << " " << k << "\n";	
+  myfile << grid.nx << " " << grid.ny << " " << grid.nz << "\n";
+  myfile << grid.x0 << " " << grid.y0 << " " << grid.z0 << "\n";
+  myfile << grid.x1 << " " << grid.y1 << " " << grid.z1 << "\n";
+  myfile << grid.dx << " " << grid.dy << " " << grid.dz << "\n";
+ 
+  cell_id = 0 * (grid.ny * grid.nz * 4) + 0 * (grid.nz * 4) + 0 * (4);
 
-  //myfile.close();
+  // print to file mappa[i,j,k,:]
+  for (s = 0; s < 4; s++) {
+        myfile << mappa[cell_id + s] << " ";
+    }
+  myfile << "\n";
+
+  cell_id = 20 * (grid.ny * grid.nz * 4) + 40 * (grid.nz * 4) + 10 * (4);
+ 
+  // print to file mappa[i,j,k,:]
+  for (s = 0; s < 4; s++) {
+        myfile << mappa[cell_id + s] << " ";
+    }
+  myfile << "\n";
+
+  myfile.close(); */
 
   // this is needed to allocate memory space for the pointers declared in the .h file in the "private" section
   memory->create(this->ftotal, atom->nmax, "FixVolumetricRestraint:ftotal");
@@ -133,6 +141,7 @@ FixVolumetricRestraint::FixVolumetricRestraint(LAMMPS *lmp, int narg, char **arg
 
 FixVolumetricRestraint::~FixVolumetricRestraint() {
   memory->destroy(ftotal);
+  delete[] mappa;
 }
 
 
@@ -151,8 +160,8 @@ int FixVolumetricRestraint::setmask() {
 
 double FixVolumetricRestraint::memory_usage()
 {
-  int tot_size =  n_voxel[0] * n_voxel[1] * n_voxel[2] * 5;
-  return tot_size * sizeof(float);
+  int tot_size =  grid.nx * grid.ny * grid.nz * 5;
+  return tot_size * sizeof(int);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -192,6 +201,7 @@ void FixVolumetricRestraint::post_force(int vflag)
   int         edt[3];    // integer indexes for the closest EDT (Euclidean Distance Transform) to the particle voxel [ix_d[0], id_x[1], id_x[2]]
   int          ix[3];    // integer indexes for the voxel where the particle sits (which factors in the possibility of the particle being outside of grid)
   double       norma;    // to save the norm of an array computed manually
+  int         indice;
 
   //std::ofstream myfile;
   //myfile.open("debug_forces.txt");
@@ -207,24 +217,26 @@ void FixVolumetricRestraint::post_force(int vflag)
       //myfile << " particle coord # " << x[i][0] << " " << x[i][1] << "  " << x[i][2] << "\n";
  
       // get the voxel indices for each particle, by rescaling the coordinates
-      ix_d[0] = ( x[i][0] - origin[0] ) / grid_step[0];
-      ix_d[1] = ( x[i][1] - origin[1] ) / grid_step[1];
-      ix_d[2] = ( x[i][2] - origin[2] ) / grid_step[2];
+      ix_d[0] = ( x[i][0] - grid.x1 ) / grid.dx;
+      ix_d[1] = ( x[i][1] - grid.y1 ) / grid.dy;
+      ix_d[2] = ( x[i][2] - grid.z1 ) / grid.dz;
+
+      //myfile << x[i][0] << " " << grid.nx << "  " << grid.ny << " " << grid.nz << "\n";
 
       //myfile << " particle voxel # " << ix_d[0] << " " << ix_d[1] << " " << ix_d[2] << "\n";
       //myfile << i << " PRE-FIX " << f[i][0] << " " << f[i][1] << " " << f[i][2] << "\n";
 
       // account for the possibility of particles floating outside of the density map range:
       // if they are, then voxel indexes are negative to immediately recognize them
-      ix[0] = ( ix_d[0] >= 0 && ix_d[0] < n_voxel[0] ) ? int(ix_d[0]) : -1;
-      ix[1] = ( ix_d[1] >= 0 && ix_d[1] < n_voxel[1] ) ? int(ix_d[1]) : -1;
-      ix[2] = ( ix_d[2] >= 0 && ix_d[2] < n_voxel[2] ) ? int(ix_d[2]) : -1;
+      ix[0] = ( ix_d[0] >= 0 && ix_d[0] < grid.nx ) ? int(ix_d[0]) : -1;
+      ix[1] = ( ix_d[1] >= 0 && ix_d[1] < grid.ny ) ? int(ix_d[1]) : -1;
+      ix[2] = ( ix_d[2] >= 0 && ix_d[2] < grid.nz ) ? int(ix_d[2]) : -1;
 
       //myfile << "particle # " << i << " voxel = " << ix[0] << " " << ix[1] << " " << ix[2] << "\n";
       //myfile << "particle # " << i << " " << mappa[ix[0]][ix[1]][ix[2]] << "\n";
 	
       // volume confinement (exerted by the nuclear lamina, for example)
-      if (body_idx == 0) 
+      if (grid.body_idx == 0) 
       {
  
          // ----------------------------------------------------------------------------------
@@ -237,26 +249,29 @@ void FixVolumetricRestraint::post_force(int vflag)
       	 // if particle is inside grid (all coordinates are positive), then we have a force given by the distance of its EDT image
          if (ix[0] >= 0 && ix[1] >= 0 && ix[2] >= 0) {
 
+              // find index
+              indice = ix[0] * (grid.ny * grid.nz * 4) + ix[1] * (grid.nz * 4) + ix[2] * 4;
+
               // if the particle is outside the nuclear volume and we have k >0 (VOLUME) OR inside the volume and we have k<0 (DAMID):
-              if ( ((mappa[ix[0]][ix[1]][ix[2]][3] == 0) && (k > 0)) || ((mappa[ix[0]][ix[1]][ix[2]][3] != 0) && (k < 0)))
+              if ( ((mappa[indice + 3] == 0) && (k > 0)) || ((mappa[indice + 3] != 0) && (k < 0)))
                 {
 			// find the EDT image of the voxel the locus is currently in
 
-              		edt[0] = mappa[ix[0]][ix[1]][ix[2]][0];
-              		edt[1] = mappa[ix[0]][ix[1]][ix[2]][1];
-              		edt[2] = mappa[ix[0]][ix[1]][ix[2]][2];
+              		edt[0] = mappa[indice + 0];
+              		edt[1] = mappa[indice + 1];
+              		edt[2] = mappa[indice + 2];
 
-	      		norma = sqrt(  grid_step[0] * grid_step[0] * (edt[0] - ix[0]) * (edt[0] - ix[0]) +
-              		               grid_step[1] * grid_step[1] * (edt[1] - ix[1]) * (edt[1] - ix[1]) +
-              		               grid_step[2] * grid_step[2] * (edt[2] - ix[2]) * (edt[2] - ix[2]) 
+	      		norma = sqrt(  grid.dx * grid.dx * (edt[0] - ix[0]) * (edt[0] - ix[0]) +
+              		               grid.dy * grid.dy * (edt[1] - ix[1]) * (edt[1] - ix[1]) +
+              		               grid.dz * grid.dz * (edt[2] - ix[2]) * (edt[2] - ix[2]) 
               		             );
 
               		ftotal[i] = k * k * norma; 
               		etotal    += k * k * norma * norma; 
 
-                      	f[i][0] += k * k * (edt[0] - ix[0])/n_voxel[0]; // * grid_step[0] * (edt_image[0] - ix[0])/norma;
-                      	f[i][1] += k * k * (edt[1] - ix[1])/n_voxel[1]; // * grid_step[1] * (edt_image[1] - ix[1])/norma;
-                      	f[i][2] += k * k * (edt[2] - ix[2])/n_voxel[2]; // * grid_step[2] * (edt_image[2] - ix[2])/norma;
+                      	f[i][0] += k * k * (edt[0] - ix[0])/(grid.nx); // * grid_step[0] * (edt_image[0] - ix[0])/norma;
+                      	f[i][1] += k * k * (edt[1] - ix[1])/(grid.ny); // * grid_step[1] * (edt_image[1] - ix[1])/norma;
+                      	f[i][2] += k * k * (edt[2] - ix[2])/(grid.nz); // * grid_step[2] * (edt_image[2] - ix[2])/norma;
 
 		 }
               }
@@ -264,62 +279,65 @@ void FixVolumetricRestraint::post_force(int vflag)
          // if particle is partially outside of the unitary cell, then a unitary radial attractive force will do the trick
          else if (k > 0) {
               // if volume, otherwise no force
-              norma = sqrt((x[i][0] - center[0]) * (x[i][0] - center[0]) +
-                           (x[i][1] - center[1]) * (x[i][1] - center[1]) +
-                           (x[i][2] - center[2]) * (x[i][2] - center[2])
+              norma = sqrt((x[i][0] - grid.x0) * (x[i][0] - grid.x0) +
+                           (x[i][1] - grid.y0) * (x[i][1] - grid.y0) +
+                           (x[i][2] - grid.z0) * (x[i][2] - grid.z0)
                           );
 
               ftotal[i] = norm3(f[i]);
               etotal    += k / 2;
 
-              f[i][0] += k * (center[0] - x[i][0]) / norma;
-              f[i][1] += k * (center[1] - x[i][1]) / norma;
-              f[i][2] += k * (center[2] - x[i][2]) / norma;
+              f[i][0] += k * (grid.x0 - x[i][0]) / norma;
+              f[i][1] += k * (grid.y0 - x[i][1]) / norma;
+              f[i][2] += k * (grid.z0 - x[i][2]) / norma;
 
 	       }                                                
       }
 
       // IF WE ARE TALKING ABOUT NUCLEAR BODY
-      if (body_idx == 1)
+      if (grid.body_idx == 1)
       {
 
           // if inside the volume grid and inside the reference volume, apply expulsion/excluded force
 	  if (ix[0] >= 0 && ix[1] >=0 && ix[2] >=0)
           {
+              // find index
+              indice = ix[0] * (grid.ny * grid.nz * 4) + ix[1] * (grid.nz * 4) + ix[2] * 4;
+               
               // ... outside nucleolar volume but to be attracted back to surface (lamina) or inside volume to be expelled
-              if (((mappa[ix[0]][ix[1]][ix[2]][3] == 1) && (k >0)) || ((mappa[ix[0]][ix[1]][ix[2]][3] == 0) && (k <0)))
+              if (((mappa[indice + 3] == 1) && (k >0)) || ((mappa[indice + 3] == 0) && (k <0)))
 	      {
 
-              		edt[0] = mappa[ix[0]][ix[1]][ix[2]][0];
-                	edt[1] = mappa[ix[0]][ix[1]][ix[2]][1];
-                	edt[2] = mappa[ix[0]][ix[1]][ix[2]][2];
+              		edt[0] = mappa[indice + 0];
+                	edt[1] = mappa[indice + 1];
+                	edt[2] = mappa[indice + 2];
               
-                	norma = sqrt( grid_step[0] * grid_step[0] * (edt[0] - ix[0]) * (edt[0] - ix[0]) +
-                              	      grid_step[1] * grid_step[1] * (edt[1] - ix[1]) * (edt[1] - ix[1]) +
-                                      grid_step[2] * grid_step[2] * (edt[2] - ix[2]) * (edt[2] - ix[2])
+                	norma = sqrt( grid.dx * grid.dx * (edt[0] - ix[0]) * (edt[0] - ix[0]) +
+                              	      grid.dy * grid.dy * (edt[1] - ix[1]) * (edt[1] - ix[1]) +
+                                      grid.dz * grid.dz * (edt[2] - ix[2]) * (edt[2] - ix[2])
                                     );
               
                 	ftotal[i] = k * k * norma;
                 	etotal    += k * k * norma * norma;
 
-                   	f[i][0] += k * k * (edt[0] - ix[0])/n_voxel[0]; //]* grid_step[0] * (edt_image[0] - ix[0])/norma;
-                   	f[i][1] += k * k * (edt[1] - ix[1])/n_voxel[1]; //grid_step[1] * (edt_image[1] - ix[1])/norma;
-                   	f[i][2] += k * k * (edt[2] - ix[2])/n_voxel[2]; //grid_step[2] * (edt_image[2] - ix[2])/norma;
+                   	f[i][0] += k * k * (edt[0] - ix[0])/grid.nx; //]* grid_step[0] * (edt_image[0] - ix[0])/norma;
+                   	f[i][1] += k * k * (edt[1] - ix[1])/grid.ny; //grid_step[1] * (edt_image[1] - ix[1])/norma;
+                   	f[i][2] += k * k * (edt[2] - ix[2])/grid.nz; //grid_step[2] * (edt_image[2] - ix[2])/norma;
 	      }
 	  }
           else if (k < 0)
                 {
-                norma =  sqrt((x[i][0] - center[0]) * (x[i][0] - center[0]) +
-                              (x[i][1] - center[1]) * (x[i][1] - center[1]) +
-                              (x[i][2] - center[2]) * (x[i][2] - center[2])
+                norma =  sqrt((x[i][0] - grid.x0) * (x[i][0] - grid.x0) +
+                              (x[i][1] - grid.y0) * (x[i][1] - grid.y0) +
+                              (x[i][2] - grid.z0) * (x[i][2] - grid.z0)
                           );
 
               ftotal[i] = norm3(f[i]);
-              etotal    += k / 2;
+              etotal    += k * k / 2;
 
-              f[i][0] += k * k * (center[0] - x[i][0]) / norma;
-              f[i][1] += k * k * (center[1] - x[i][1]) / norma;
-              f[i][2] += k * k * (center[2] - x[i][2]) / norma;
+              f[i][0] += k * k * (grid.x0 - x[i][0]) / norma;
+              f[i][1] += k * k * (grid.y0 - x[i][1]) / norma;
+              f[i][2] += k * k * (grid.z0 - x[i][2]) / norma;
                 }
      }
 
--- src/USER-GENOME/fix_volumetric_restraint.h
+++ src/USER-GENOME/fix_volumetric_restraint.h
@@ -24,6 +24,12 @@ FixStyle(volumetricrestraint,FixVolumetricRestraint)
 
 namespace LAMMPS_NS {
 
+struct grid_info {
+   int body_idx, nx, ny, nz;    // body_idx, number of voxels
+   float x0, y0, z0;            // grid center
+   float x1, y1, z1;            // origin: lower leftmost corner
+   float dx, dy, dz;            // grid steps
+};
 
 class FixVolumetricRestraint : public Fix {
  public:
@@ -40,24 +46,12 @@ class FixVolumetricRestraint : public Fix {
   double compute_vector(int);
 
  private:
-  double            k, evf;  // elastic constant, factor to scale up/down any nuclear/nucleolar envelope
-  double         origin[3];   // [x_min, y_min, z_min] origin, lowest, leftmost vertex for the volume map
-  double      grid_step[3]; // [dx, dy, dz] grid spacing (aka, voxel size) along the x, y and z binning
-  double         center[3];    // geometric center of the nuclear body, pozzo del campo centrale di forza
-  int           n_voxel[3];    // number of voxels along the three dimensions
-  int             body_idx;     // index to discriminate between nuclear envelope and/ord nuclear bodies
- 
-  int               mappa[500][410][120][4];   // per il momento e' inizializzata in eccesso...cioe' e' piu' grande di quello che serve (dynamic stuff)?
-					       // dynamic allocation is possible using pointers of pointers etc...but it will be much slower than just 
-					       // initializing a larger-than-needed static array.
-					       // Guido's version used dynamic allocation: see "fix_volumetric_smart.cpp" in /u/home/b/bonimba/lammps_
-					       // spare_code for the code to allocate and de-allocate dynamically, after nvoxel array is read in.
-					       // Also, it might help improve performance and data storage if we used a c++ libray to read npz files.
-					       // Instead of plain txt files. This is a technical improvement.
 
-
-  double            etotal;
-  double*           ftotal;
+  double   k, evf; 
+  grid_info  grid;   // struct object which contains grid parameters
+  int   *mappa;           
+  double   etotal;
+  double*  ftotal;
 };
 
 }
